b4fb6d14d0294283da2067182cd3312cbaaf71f8
Karolis Ramanauskas <karolisr@live.com>
2017-08-17 17:14:05 +0300

    Update FAQ.

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -33,26 +33,52 @@ class Header extends Component {
 
           <div className="modal-body">
             <h3>Who are you?</h3>
-            <p>Nexchange is an automated cryptocurrency exchange service, operated by YOA LTD. (registered in United Kingdom).</p>
+            <p>Nexchange is an automated cryptocurrency exchange service, operated by <a href="https://beta.companieshouse.gov.uk/company/10009845">YOA LTD. (registered in United Kingdom)</a>.</p>
             <p>Our customer support phone number is +442081442192.</p>
-
-            <h3>What is your fee?</h3>
-            <p>Our service fee is 0.5%. No hidden fees here, the amount you see on the screen is the exact amount you will be getting in your wallet.</p>
 
             <h3>What do you do?</h3>
             <p>We allow you to exchange one cryptocurrency for another.</p>
-            <p>Right now we support Bitcoin (BTC), Ethereum (ETC) and Litecoin (LTC).</p>
-            <p>We will be adding more currencies in the near future, stay tuned!</p>
+            <p>Currently, we support Bitcoin (BTC), Ethereum (ETC) and Litecoin (LTC).</p>
+            <p>We will be adding more currencies very soon, stay tuned!</p>
             <p>Missing your favorite coin? Let us know here: <a href="mailto:support@nexchange.co.uk">support@nexchange.co.uk</a>.</p>
 
-            <h3>How long does it take to convert?</h3>
-            <p>Initiation a transaction doesn’t take more than a couple of minutes. Actual processing takes about 5-10 minutes before you receive your coins.</p>
+            <h3>What is your fee?</h3>
+            <p>Our service fee is 0.5%. There are no hidden fees here: the amount you see on the screen is the exact amount you get in your wallet.</p>
+
+            <h3>How long does a conversion take?</h3>
+            <p>Initiating a transaction doesn’t take more than a couple of minutes. The actual processing takes about 5–10 minutes before you receive your coins in your desired currency.</p>
+
+            <h3>How does it work?</h3>
+            <p>Here is how you use the exchange:</p>
+            <ol>
+              <li><b>Enter</b> your desired <b>amount, choose</b> the currency type you <b>want to spend</b>, and the currency type you <b>want to buy</b> (e.g. if you want to spend 1 BTC to buy ETH at the exchange rate shown, you enter “1” and click on “GET STARTED”)</li>
+              <li><b>Enter your destination address</b> (e.g. if you are buying ETH, you enter your ETH wallet address)</li>
+              <li>You will be prompted to <b>send</b> your Bitcoins to a <b>specified address</b>.</li>
+              <li>Once we receive your coins, the processing of your order will start immediately.</li>
+              <li>Once everything is done, you will receive your ETH. Hurray! You just successfully exchanged your cryptocurrency.</li>
+            </ol>
+            <p>Once you have paid for your order, it cannot be reversed. Please ensure that you have put in the right address.</p>
+            <p>In case of an issue, your order will be refunded to your original wallet.</p>
 
             <h3>Do you collect any private information?</h3>
             <p>We do not collect or store any private information about you. The process is completely anonymous.</p>
 
             <h3>Are you using a third-party exchange or API?</h3>
             <p>No, our solution is completely independant. We use our internal coin reserves to provide liquidity.</p>
+
+            <h3>How do you manage security?</h3>
+            <p>We do not provide wallet hosting service at the moment. The exchange happens between two of your wallets:</p>
+            <ol>
+              <li>The wallet you send the funds from (<b>spending</b> wallet)</li>
+              <li>The wallet you receive funds to (<b>receiving</b> wallet)</li>
+            </ol>
+            <p>Once the transaction has been processed, you are in charge of your wallet balance security.</p>
+
+            <h3>How do I track my order?</h3>
+            <p>Every order has a unique id that looks like this: INSERT PICTURE</p>
+            <p>Make sure you record your order id. We recommend bookmarking the page, you can always navigate to it later.</p>
+            <p>Once you have sent the cryptocurrency, losing connection, closing the tab or navigating elsewhere will not impact the transaction.</p>
+            <p>If you need help, feel free to contact us.</p>
           </div>
 
           <div className="modal-footer">
