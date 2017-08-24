f61b00a61ee151a7f341fd6cfaf8fb1dc6d80a36
Karolis Ramanauskas <karolisr@live.com>
2017-08-24 15:44:39 +0300

    Merge

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 import Main from '../components/Main';
 import Team from '../components/Team';
+import RecentOrders from '../components/RecentOrders';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
+        <RecentOrders />
         <Team />
       </div>
     );
