37ebf9009aacb92411b69d66fdd7e5f20468220c
Karolis Ramanauskas <karolisr@live.com>
2017-08-28 19:03:25 +0300

    FAQ adjustments. About us description adjustment.

--- src/components/Team.js
+++ src/components/Team.js
@@ -7,13 +7,15 @@ class Team extends Component {
     return (
       <div id="team" className="container">
         <div className="row">
-          <div className="col-xs-12">
+          <div id="team-description" className="col-xs-12">
             <h3>About Us</h3>
 
             <p>Nexchange is a fast, reliable and fully transparent cryptocurrency exchange built by crypto enthusiasts, for crypto enthusiasts.</p>
-            <p>We are a group of multi-national, multi-disciplinary, multi-planetary (just kidding) cryptocurrency professionals. Our team came together to make this platform after seeing firsthand the need for a fast and reliable exchange on the crypto market.</p>
-            <p>We pride ourselves on total transparency: Everything is viewable through our API, starting from order lists and ending with our coin reserves.</p>
-            <p>To support our mission of making crypto accessible to everyone, we’ve made our <a href="https://github.com/onitsoft/nexchange-open-client-react" target="_blank">front end client completely open source</a>.</p>
+            <p>We are a group of multi-national, multi-disciplinary cryptocurrency professionals.</p>
+            <p>Our team came together to make this platform after seeing firsthand the need for a fast and reliable exchange on the crypto market.</p>
+            <p>We pride ourselves on total transparency: Everything is viewable through our API, starting from order lists and ending with our coin reserves. </p>
+            <p>Nexchange is built on several core values that guide our team in our day to day operations. Among them are complete transparency, a strive to help make cryptocurrency as accessible as possible and a passion for open source.</p>
+            <p>To support our mission of making crypto accessible to everyone, we’ve made our <b><a href="https://github.com/onitsoft/nexchange-open-client-react" target="_blank">front end client completely open source <i className="fa fa-github" aria-hidden="true"></i></a></b>.</p>
           </div>
         </div>
 
