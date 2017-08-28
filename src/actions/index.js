d79611cc3f9c37c49d37433a0de5ed55f0272efc
Karolis Ramanauskas <karolisr@live.com>
2017-08-28 12:27:25 +0300

    Initial dynamic data loading from backend for coins data.

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,14 +18,6 @@ export function setWallet(payload) {
 }
 
 export function selectCoin(payload) {
-	// DISPATCH COIN_SELECTED AND
-	// THEN UPDATE AMOUNTS
-	// ACTUALLY, WRONG PLACE, SHOULD
-	// DETECT SELECTED COIN PROPS CHANGE AND THEN
-	// UPDATE AMOUNTS
-	// return (dispatch, getState) {
-	// }
-
 	return {
 		type: 'COIN_SELECTED',
 		payload: payload
@@ -37,6 +29,36 @@ export function updateAmounts(payload) {
 		type: 'UPDATE_AMOUNTS',
 		payload: payload
 	}
+}
+
+export function fetchCoinDetails(payload) {
+	const url = `${config.API_BASE_URL}/currency/`;
+	const request = axios.get(url);
+
+    return (dispatch, getState) => {
+        request
+	        .then(response => {
+	        	if (!response.data.length) return;
+
+	        	console.log(response.data);
+
+	        	// dispatch({type: 'PRICE_FETCHED', payload: {
+	        	// 	price: response.data[0].ticker.ask,
+	        	// 	pair: payload.pair,
+	        	// 	lastFetched: new moment()
+	        	// }});
+
+	        	// if (payload.amount) {
+		        // 	dispatch({type: 'UPDATE_AMOUNTS', payload: {
+		        // 		price: response.data[0].ticker.ask,
+		        // 		amount: payload.amount,
+		        // 		lastEdited: payload.lastEdited,
+		        // 	}});
+	        	// }
+	        }).catch(error => {
+	        	console.log(error);
+	        });    
+    };
 }
 
 export function fetchPrice(payload) {
