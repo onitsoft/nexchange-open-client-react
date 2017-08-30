4178440b30cfd462cb77fc67c62ffe2da15279f4
Karolis Ramanauskas <karolisr@live.com>
2017-08-30 16:19:22 +0300

    Fix coin amount calculation when amount is deleted.

--- src/reducers/reducer_update_amounts.js
+++ src/reducers/reducer_update_amounts.js
@@ -4,28 +4,17 @@ let initialState = {
 	lastEdited: null
 }
 
-function getDecimalPlaces (amount) {
-    let decimalPlaces = 2,
-        invertedDecimalSize = -Math.floor(Math.log10(amount));
-
-    if (invertedDecimalSize > 0)
-        decimalPlaces = decimalPlaces + invertedDecimalSize;
-
-    return decimalPlaces;
-}
-
 export default (state = initialState, action) => {
 	if (action.type == 'UPDATE_AMOUNTS') {
 		let newState = Object.assign({}, state),
 			opposite = (action.payload.lastEdited == 'receive' ? 'deposit' : 'receive'),
 			price = (action.payload.price ? action.payload.price : state.price),
-			quote = (action.payload.amount ? action.payload.amount : state[action.payload.lastEdited]),
-			sum = (action.payload.lastEdited == 'receive' ? parseFloat(quote) * price : parseFloat(quote) / price),
-			decimalPlaces = getDecimalPlaces(sum);
+			quote = (action.payload.amount ? action.payload.amount : (action.payload.amount == "" ? 0 : state[action.payload.lastEdited])),
+			sum = (action.payload.lastEdited == 'receive' ? parseFloat(quote) * price : parseFloat(quote) / price);
 
 		newState['lastEdited'] = action.payload.lastEdited;
 		newState[action.payload.lastEdited] = action.payload.amount;
-		newState[opposite] = (!isNaN(decimalPlaces) && !isNaN(sum) && isFinite(decimalPlaces) ? sum.toFixed(8) : '...');
+		newState[opposite] = (!isNaN(sum) && isFinite(sum) && sum > 0 ? sum.toFixed(8) : '...');
 
 		return newState;
 	}
