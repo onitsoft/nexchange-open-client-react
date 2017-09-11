6c76dc2189aaccf6b79218a302bd4d72eaf7a73f
Karolis Ramanauskas <karolisr@live.com>
2017-09-11 18:22:36 +0300

    Add back proper About Us description.

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,11 @@ const Team = () => (
         <div id="team-description" className="col-xs-12">
           <h2>About Us</h2>
 
-          <p>Funchange is a fast, reliable and fully transparent cryptocurrency exchange built by crypto enthusiasts, for crypto enthusiasts.</p>
+          <p>Nexchange is a fast, reliable and fully transparent cryptocurrency exchange built by crypto enthusiasts, for crypto enthusiasts.</p>
           <p>We are a group of multi-national, multi-disciplinary cryptocurrency professionals.</p>
           <p>Our team came together to make this platform after seeing firsthand the need for a fast and reliable exchange on the crypto market.</p>
           <p>We pride ourselves on total transparency: Everything is viewable through our API, starting from order lists and ending with our coin reserves. </p>
-          <p>Funchange is built on several core values that guide our team in our day to day operations. Among them are complete transparency, a strive to help make cryptocurrency as accessible as possible and a passion for open source.</p>
+          <p>Nexchange is built on several core values that guide our team in our day to day operations. Among them are complete transparency, a strive to help make cryptocurrency as accessible as possible and a passion for open source.</p>
           <p>To support our mission of making crypto accessible to everyone, we’ve made our <b><a href="https://github.com/onitsoft/nexchange-open-client-react" target="_blank">front end client completely open source <i className="fa fa-github" aria-hidden="true"></i></a></b>.</p>
 
           <div id="team-members" className="row">
