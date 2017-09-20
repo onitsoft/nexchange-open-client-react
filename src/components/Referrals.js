45a77a0e0b3ddce9a9b9496470d3edd0254d814e
Karolis Ramanauskas <karolisr@live.com>
2017-09-20 16:44:30 +0300

    Add back old x-referral-token header.

--- src/components/Referrals.js
+++ src/components/Referrals.js
@@ -16,7 +16,7 @@ class Referrals extends Component {
 			let referral = (config.REFERRAL_CODE ? config.REFERRAL_CODE : localStorage.getItem('referral'));
 
 		    if (referral && requestConfig.url && requestConfig.url.indexOf(config.API_BASE_URL.toLowerCase()) > -1)
-			    requestConfig.headers['HTTP_X_REFERRAL_TOKEN'] = referral;
+			    requestConfig.headers['x-referral-token'] = referral;
 
 		    return requestConfig;
 		  }, function (error) {
