d46325f554773d879c4fa3c053ae8d6425369485
Karolis Ramanauskas <hello@karolisram.com>
2017-09-27 11:58:27 +0300

    Tx id overflow fix. Add tx blockchain URL. Adjust font for referral header.

--- src/components/OrderPreReleased.jsx
+++ src/components/OrderPreReleased.jsx
@@ -6,6 +6,7 @@ const OrderPreReleased = (props) => (
 	<div className="col-xs-12 text-center order-status-section">
 		<h2 style={{margin: "0"}}>We are processing your order</h2>
 		<h5>Please allow up to 15 minutes.</h5>
+		
 		<a href={`${config.API_BASE_URL}/orders/${props.orderRef}`} target="_blank"><h4 style={{margin: "25px 0 0px", "fontWeight": "500"}}>See your order details on our API</h4></a>
 	</div>
 );
