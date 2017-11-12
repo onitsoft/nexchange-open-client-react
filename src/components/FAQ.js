67812d4c94e23a9165eb8f0d96ae78178980c32c
Oleg Belousov <oleg@nexchange.co.uk>
2017-11-12 22:07:33 +0000

    Update FAQ.js

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -35,7 +35,7 @@ class FAQ extends Component {
               question="Who are you?"
               answer={
                 <div>
-                  <p>Nexchange is an automated cryptocurrency exchange service, operated by <a href="https://beta.companieshouse.gov.uk/company/10009845" target="_blank">Ontarion Enterprise Sp. Z. O. O. (registered in Warsaw, Poland)</a>.</p>
+                  <p>Nexchange is an automated cryptocurrency exchange service, operated by <a href="http://www.krs-online.com.pl/ontario-enterprise-sp-z-o-o-krs-6221455.html" target="_blank">Ontarion Enterprise Sp. Z. O. O. (registered in Warsaw, Poland)</a>.</p>
                   <p>Our customer support phone number in UK is <a href="tel:+442081442192">+442081442192</a>.</p>
                 </div>
               }/>
