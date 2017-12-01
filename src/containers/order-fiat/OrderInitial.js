910fc8cbd467d9473902c8dbe2d58a062b1d34ae
Karolis Ramanauskas <hello@karolisram.com>
2017-12-01 18:27:40 +0200

    Display fee that is taken from the receive amount

--- src/containers/order-fiat/OrderInitial.js
+++ src/containers/order-fiat/OrderInitial.js
@@ -17,7 +17,9 @@ class OrderInitial extends Component {
 
     			<div id="order-payment-details" className="col-xs-12 col-ms-7 col-sm-7 col-md-9">
     				<h3>Time remaining: <span id="time-remaining"><b>{this.props.timeRemaining}</b></span></h3>
-    				<h4>Pay <b>{parseFloat(this.props.order.amount_quote)} {this.props.order.pair.quote.code}</b></h4>
+    				<h4>Pay <b>{parseFloat(this.props.order.amount_quote)} {this.props.order.pair.quote.code}</b> <br />
+    					Fee amounts to <b>{parseFloat(this.props.order.amount_quote_fee)} {this.props.order.pair.quote.code}</b>
+    				</h4>
 
 					<a href={this.props.order.payment_url} target="_blank" className="btn btn-default btn-themed">
 						<i className="fa fa-credit-card" aria-hidden="true" style={{position: "relative", left: -13}}></i>
