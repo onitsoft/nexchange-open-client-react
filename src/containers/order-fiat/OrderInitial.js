b366b1cfeeb271addbbd2d1c554b76ebb26a0660
Karolis Ramanauskas <hello@karolisram.com>
2017-12-05 11:56:16 +0200

    Upgrade card payment images and layout.

--- src/containers/order-fiat/OrderInitial.js
+++ src/containers/order-fiat/OrderInitial.js
@@ -11,11 +11,7 @@ class OrderInitial extends Component {
 	render() {
 	    return (
 	    	<div id="order-payment">
-    			<div className="col-xs-12 col-ms-5 col-sm-5 col-md-3 text-center">
-    				<img src="/img/cards.jpg" />
-    			</div>
-
-    			<div id="order-payment-details" className="col-xs-12 col-ms-7 col-sm-7 col-md-9">
+    			<div id="order-payment-details" className="col-xs-12 col-ms-6 col-sm-6 col-md-4">
     				<h3>Time remaining: <span id="time-remaining"><b>{this.props.timeRemaining}</b></span></h3>
     				<h4>Pay <b>{parseFloat(this.props.order.amount_quote)} {this.props.order.pair.quote.code}</b> <br />
     					Fee amounts to <b>{parseFloat(this.props.order.amount_quote_fee)} {this.props.order.pair.quote.code}</b>
@@ -25,6 +21,18 @@ class OrderInitial extends Component {
 						<i className="fa fa-credit-card" aria-hidden="true" style={{position: "relative", left: -13}}></i>
 						Pay now
 					</a>
+    			</div>
+
+    			<div id="cards" className="col-xs-12 col-ms-6 col-sm-6 col-md-8">
+    				<h3>Accepted cards:</h3>
+
+    				<div className="visible-xs-block visible-sm-block">
+    					<img src="/img/cards2.png" />
+    				</div>
+
+    				<div className="visible-md-block visible-lg-block">
+    					<img src="/img/cards3.png" />
+    				</div>
     			</div>
 		    </div>
 	    );
