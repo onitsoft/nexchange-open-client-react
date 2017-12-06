8b488f8a652e22ce693fd3b1dc62ee18ee5badc9
superkarolis <hello@karolisram.com>
2017-12-06 18:22:26 +0200

    Add policies to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';;
+
 
 const Footer = () => (
 	<footer>
 		<div className="container">
-		  <p className="text-muted">All rights reserved, YOA LTD 2016-2017, England & Wales <a href="https://beta.companieshouse.gov.uk/company/10009845" target="_blank">registered company No. 10009845</a></p>
+			<ul>
+				<li><Link to="/terms-and-conditions">Terms and Conditions</Link></li>
+				<li><Link to="/privacy">Privacy Policy</Link></li>
+				<li><Link to="/refund-cancellation">Refund and Cancellation Policy</Link></li>
+			</ul>
+
+			<p className="text-muted">All rights reserved, YOA LTD 2016-2017, England & Wales <a href="https://beta.companieshouse.gov.uk/company/10009845" target="_blank">registered company No. 10009845</a></p>
 		</div>
 	</footer>
 );
