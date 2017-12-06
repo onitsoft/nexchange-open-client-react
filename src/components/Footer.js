fcb6b958434aee90257148d22ffd655a8574cf8a
superkarolis <hello@karolisram.com>
2017-12-06 16:58:29 +0200

    Update footer to show compliancy info.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
 	<footer>
 		<div className="container">
-		  <p className="text-muted">&copy; Nexchange Open Client BETA</p>
+		  <p className="text-muted">All rights reserved, YOA LTD 2016-2017, England & Wales <a href="https://beta.companieshouse.gov.uk/company/10009845" target="_blank">registered company No. 10009845</a></p>
 		</div>
 	</footer>
 );
