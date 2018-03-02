c17a8b000e1042cdf058aa481a493f73b0b0ac3a
Šarūnas Ažna <sarunas@nexchange.co.uk>
2018-03-02 17:04:12 +0100

    oauth2 token on order and support

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
 	NAME: 'Nexchange',
 	DOMAIN: 'https://nexchange.io',
-	API_BASE_URL: 'https://api.nexchange.io/en/api/v1',
+	API_BASE_URL: 'http://localhost:8000/en/api/v1',
 	SUPPORT_EMAIL: 'support@nexchange.io',
 	PRICE_FETCH_INTERVAL: 60000,
 	ORDER_DETAILS_FETCH_INTERVAL: 20000,
