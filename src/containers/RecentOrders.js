1ef0109c1cedc9fc08624911d2a1ec68f2dfc833
Mr <looking.a.new.job@gmail.com>
2018-03-18 01:21:58 +0100

    localization for RecentOrders.js
    Added a moment locale for easy translations

--- src/containers/RecentOrders.js
+++ src/containers/RecentOrders.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import moment from 'moment';
+import 'moment/locale/en-gb';
 import _ from 'lodash';
 
 import Helpers from '../helpers';
