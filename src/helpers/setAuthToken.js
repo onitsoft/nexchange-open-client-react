6a3fde548b33552c241561db2df548be08010bcf
superkarolis <hello@karolisram.com>
2018-04-11 20:04:57 +0300

    Now send authorization header for KYC endpoint

--- src/helpers/setAuthToken.js
+++ src/helpers/setAuthToken.js
@@ -3,7 +3,9 @@ import config from '../config';
 
 export default () => {
 	axios.interceptors.request.use(function (requestConfig) {
-		if (requestConfig.url.indexOf(config.API_BASE_URL) > -1 && localStorage.token) {
+		if (requestConfig.url.indexOf(config.API_BASE_URL) > -1
+			&& requestConfig.url.indexOf('kyc') === -1
+			&& localStorage.token) {
 			const token = localStorage.token;
 			requestConfig.headers['Authorization'] = `Bearer ${token}`;
 		}
