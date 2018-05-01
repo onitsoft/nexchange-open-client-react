aa4e14a80a5a3a573799a651b84139a97bb572b7
superkarolis <hello@karolisram.com>
2018-05-01 11:36:31 +0300

    Add missing oxford comma.

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => (
           <p>We are a group of multi-national, multi-disciplinary cryptocurrency professionals.</p>
           <p>Our team came together to make this platform after seeing firsthand the need for a fast and reliable exchange on the crypto market.</p>
           <p>We pride ourselves on total transparency: Everything is viewable through our API, starting from order lists and ending with our coin reserves. </p>
-          <p>N.exchange is built on several core values that guide our team in our day to day operations. Among them are complete transparency, a strive to help make cryptocurrency as accessible as possible and a passion for open source.</p>
+          <p>N.exchange is built on several core values that guide our team in our day to day operations. Among them are complete transparency, a strive to help make cryptocurrency as accessible as possible, and a passion for open source.</p>
           <p>To support our mission of making crypto accessible to everyone, we’ve made our <b><a href="https://github.com/onitsoft/nexchange-open-client-react" target="_blank">front end client completely open source <i className="fa fa-github" aria-hidden="true"></i></a></b>.</p>
 
           <div id="team-members" className="row">
