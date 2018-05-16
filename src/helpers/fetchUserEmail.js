138e107c39fa9653f66ff4ec734399ee28af4deb
Karolis Ramanauskas <hello@karolisram.com>
2018-05-16 18:08:07 +0300

    Do not send /users/me request if no localStorage is present. Only applies to GET.

--- src/helpers/fetchUserEmail.js
+++ src/helpers/fetchUserEmail.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import config from '../config';
 
 export default (successCb, errorCb) => {
+  if (!localStorage.token) return;
+
   axios
     .get(`${config.API_BASE_URL}/users/me/`)
     .then(response => {
