ccc126004d788f971d2110e015996b87e08a814e
Karolis Ramanauskas <hello@karolisram.com>
2018-05-17 13:58:54 +0300

    intermediary changes. Undo later.

--- src/containers/order-fiat/OrderPayment.js
+++ src/containers/order-fiat/OrderPayment.js
@@ -16,6 +16,7 @@ class OrderPayment extends Component {
   }
 
   static getDerivedStateFromProps(nextProps) {
+    console.log(nextProps.kyc);
     return nextProps;
   }
 
@@ -47,23 +48,18 @@ class OrderPayment extends Component {
     if (!this.props.kyc.is_verified) {
       const { residence_document_status, id_document_status } = this.props.kyc;
 
-      if (
-        id_document_status === 'UNDEFINED' &&
-        residence_document_status === 'UNDEFINED'
-      ) {
+      if (id_document_status === 'UNDEFINED' && residence_document_status === 'UNDEFINED') {
         title = <h2>Awaiting verification</h2>;
         inner = (
           <div>
             <h5>
-              In order to proceed further we must get to know you better by
-              getting a copy of your government issued ID and a proof of
+              In order to proceed further we must get to know you better by getting a copy of your government issued ID and a proof of
               residence.
             </h5>
 
             <h5 style={{ marginTop: 15 }}>
               <b>
-                This is a one-time process, once verified you’ll be able to
-                complete future purchases instantly until current verification
+                This is a one-time process, once verified you’ll be able to complete future purchases instantly until current verification
                 tier limit is reached.
               </b>
             </h5>
@@ -90,10 +86,7 @@ class OrderPayment extends Component {
         notificationsCtaVisible = true;
         modal = KYCModalTier0;
 
-        if (
-          id_document_status === 'REJECTED' ||
-          residence_document_status === 'REJECTED'
-        ) {
+        if (id_document_status === 'REJECTED' || residence_document_status === 'REJECTED') {
           buttonText = 'Retry verification';
         }
       }
@@ -101,10 +94,7 @@ class OrderPayment extends Component {
       title = <h2>Tier limits reached, additional verification needed</h2>;
 
       const tier = this.props.kyc.limits_message.tier.name;
-      const {
-        selfie_document_status,
-        whitelist_selfie_document_status,
-      } = this.props.kyc;
+      const { selfie_document_status, whitelist_selfie_document_status } = this.props.kyc;
 
       if (tier === 'Tier 1') {
         modal = KYCModalTier1;
@@ -121,8 +111,7 @@ class OrderPayment extends Component {
             <h5>{this.props.kyc.limits_message.tier.upgrade_note}</h5>
             <h5 style={{ marginTop: 15 }}>
               <b>
-                This is a one-time process, once verified you’ll be able to
-                complete future purchases instantly until current verification
+                This is a one-time process, once verified you’ll be able to complete future purchases instantly until current verification
                 tier limits are reached.
               </b>
             </h5>
@@ -150,10 +139,7 @@ class OrderPayment extends Component {
           </div>
         );
 
-        if (
-          selfie_document_status === 'REJECTED' ||
-          whitelist_selfie_document_status === 'REJECTED'
-        ) {
+        if (selfie_document_status === 'REJECTED' || whitelist_selfie_document_status === 'REJECTED') {
           buttonText = 'Retry verification';
         }
 
@@ -180,7 +166,6 @@ class OrderPayment extends Component {
 
 const mapStateToProps = ({ kyc }) => ({ kyc });
 
-const mapDistachToProps = dispatch =>
-  bindActionCreators({ fetchKyc }, dispatch);
+const mapDistachToProps = dispatch => bindActionCreators({ fetchKyc }, dispatch);
 
 export default connect(mapStateToProps, mapDistachToProps)(OrderPayment);
