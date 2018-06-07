36e5ef780e0c99e9d49e1351deb9ae63f09a1936
Karolis Ramanauskas <hello@karolisram.com>
2018-06-07 18:53:45 +0300

    Order email set and referral logic set up.

--- src/components/Order/OrderCta/OrderNotifications/OrderNotifications.js
+++ src/components/Order/OrderCta/OrderNotifications/OrderNotifications.js
@@ -1,85 +1,27 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { setUserEmail } from 'Actions';
 import GetNotified from './images/get-notified.png';
-import config from 'Config';
 import styles from '../OrderCta.scss';
 
 class OrderNotifications extends Component {
-  state = {
-    email: '',
-    message: {
-      text: '',
-      error: false,
-    },
-    show: false,
-  };
-
-  componentDidMount() {
-    axios
-      .get(`${config.API_BASE_URL}/users/me/orders/${this.props.order.unique_reference}`)
-      .then(data => {
-        this.setState({ show: true });
-      })
-      .catch(error => {
-        this.setState({ show: false });
-      });
-
-    if (this.props.email.value) {
-      this.setState({
-        email: this.props.email.value,
-        emailFetched: true,
-      });
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.email !== this.props.email) {
-      if (this.props.email.message) {
-        this.setState({ message: this.props.email.message });
-      }
-
-      if (this.props.email.value) {
-        this.setState({
-          email: this.props.email.value,
-          emailFetched: true,
-        });
-      }
-    }
-  }
-
-  handleChange = event => {
-    this.setState({
-      email: event.target.value,
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.setUserEmail(this.state.email);
-  };
-
   renderForm() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.props.handleSubmit}>
         <div className={styles['form-group']}>
           <input
             type="email"
             name="email"
             placeholder="Enter your e-mail address"
-            onChange={this.handleChange}
-            value={this.state.email}
-            disabled={this.state.emailFetched}
+            onChange={this.props.handleChange}
+            value={this.props.email}
+            disabled={this.props.emailFetched}
             required
           />
 
-          <button type="submit" className={`btn btn-primary ${styles.btn}`} disabled={this.state.emailFetched}>
+          <button type="submit" className={`btn btn-primary ${styles.btn}`} disabled={this.props.emailFetched}>
             Submit
           </button>
 
-          {this.state.message.text && <h4 className={styles.message}>{this.state.message.text}</h4>}
+          {this.props.message.text && <h4 className={styles.message}>{this.props.message.text}</h4>}
         </div>
       </form>
     );
@@ -109,10 +51,4 @@ class OrderNotifications extends Component {
   }
 }
 
-const mapStateToProps = ({ email }) => ({ email });
-const mapDistachToProps = dispatch => bindActionCreators({ setUserEmail }, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDistachToProps
-)(OrderNotifications);
+export default OrderNotifications;
