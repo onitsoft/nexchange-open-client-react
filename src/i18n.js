dc8d7652eda92aa15722de650591dfa8603aa29b
Aracher17 <die.drei99@yahoo.de>
2018-06-11 01:50:12 +0200

    fixed for testing, added mock for i18next

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,4 @@
-import * as i18n from 'i18next';
+import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import { reactI18nextModule } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
