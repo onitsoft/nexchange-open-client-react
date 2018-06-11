4ada509c2400bf488c61399a70317d4a73c16593
hannojg <h.gdk@cernischi-und-gdecke.de>
2018-06-11 23:20:39 +0200

    Added testimonials, fixed missing translations in about

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Testimonial from './Testimonial/Testimonial';
+import { I18n } from 'react-i18next';
 
 const Testimonials = props => (
+ <I18n ns="translations">
+  {(t) => (
   <div id="testimonials">
     <div className="container">
       <div className="row">
         <div className="col-xs-12">
-          <h2>Testimonials</h2>
+          <h2>{t('testimonials.title')}</h2>
         </div>
       </div>
 
@@ -16,8 +19,7 @@ const Testimonials = props => (
           name="Crypto Talk‏"
           network="twitter"
           link="https://twitter.com/bitreff/status/907478815890325504"
-          text="Check Out @NexchangeIO with a Simple, Secure and a transparent
-						exchange that you use without registration."
+          text={t('testimonials.1')}
           date="September 11, 2017, 10:39 PM"
         />
         <Testimonial
@@ -25,9 +27,7 @@ const Testimonials = props => (
           name="John Michael"
           network="facebook"
           link="https://www.facebook.com/rockztar.jm/posts/1475653122471191:0"
-          text="N.exchange is the next big exchange… clean ui, mobile friendly,
-						smooth transaction and transparent platform. I have tried different types
-						exchanges but this platform delivers what I want…"
+          text={t('testimonials.2')}
           date="September 11, 2017, 01:49 PM"
         />
         <Testimonial
@@ -35,7 +35,7 @@ const Testimonials = props => (
           name="Tomo Adria"
           network="facebook"
           link="https://www.facebook.com/permalink.php?story_fbid=1884024031850695&id=100007293495907&substory_index=0"
-          text="Best new instant exchange service! Fast, easy, low fees..."
+          text={t('testimonials.3')}
           date="September 9, 2017, 22:30 PM"
         />
         <Testimonial
@@ -43,13 +43,14 @@ const Testimonials = props => (
           name="Hunterk13"
           network="bitcointalk"
           link="https://bitcointalk.org/index.php?topic=2165808.msg21736006#msg21736006"
-          text="I traded $24,000 worth of BTC to LTC yesterday, great trustworthy exchange.
-						Had a few hiccups but  everything was sorted out. Would use again. 😊"
+          text={t('testimonials.4')}
           date="September 11, 2017, 07:51 PM"
         />
       </div>
     </div>
   </div>
+ )}
+ </I18n>
 );
 
 export default Testimonials;
