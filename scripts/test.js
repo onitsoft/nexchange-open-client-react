728d930ef6cadf2fdaebea4f8d89bfabd251e73f
Karolis Ramanauskas <hello@karolisram.com>
2018-06-12 18:32:21 +0300

    Fix config for tests.

--- scripts/test.js
+++ scripts/test.js
@@ -11,7 +11,7 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('Config/env');
+require('../config/env');
 
 const jest = require('jest');
 let argv = process.argv.slice(2);
