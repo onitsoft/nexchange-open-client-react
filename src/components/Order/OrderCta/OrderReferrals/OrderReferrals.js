58952754d2af3318e7596de5089287864d12dd7e
Karolis Ramanauskas <hello@karolisram.com>
2018-06-20 16:24:14 +0300

    Update order referrals section.

--- src/components/Order/OrderCta/OrderReferrals/OrderReferrals.js
+++ src/components/Order/OrderCta/OrderReferrals/OrderReferrals.js
@@ -32,7 +32,16 @@ class OrderReferrals extends Component {
     return (
       <div>
         <div className={styles['form-group']}>
-          <input className={styles['referral-input']} type="text" value={this.state.link} disabled={true} required />
+          <CopyToClipboard text={this.state.link} onCopy={() => this.triggerCopyTooltip()}>
+            <input
+              className={styles['referral-input']}
+              type="text"
+              value={this.state.link}
+              title="Click to copy!"
+              onFocus={e => e.target.blur()}
+              required
+            />
+          </CopyToClipboard>
 
           <CopyToClipboard text={this.state.link} onCopy={() => this.triggerCopyTooltip()}>
             <button id="copy-to-clipboard-link" type="button" className={`btn btn-primary ${styles.btn}`}>
