84932c82495d062ffad4ef27550af9f5b96ebb29
Karolis Ramanauskas <hello@karolisram.com>
2018-06-23 16:05:41 +0300

    Complete testimonials section.

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -1,55 +1,75 @@
-import React from 'react';
-import Testimonial from './Testimonial/Testimonial';
+import React, { Component } from 'react';
+import Swiper from 'react-id-swiper';
+import styles from './Testimonials.scss';
+import data from './data.json';
+require('react-id-swiper/src/styles/css/swiper.css');
 
-const Testimonials = props => (
-  <div id="testimonials">
-    <div className="container">
-      <div className="row">
-        <div className="col-xs-12">
-          <h2>Testimonials</h2>
+class Testimonials extends Component {
+  swiper = null;
+
+  goNext = () => {
+    console.log(this, this.swiper);
+    if (this.swiper) this.swiper.slideNext();
+  };
+
+  goPrev = () => {
+    console.log(this, this.swiper);
+    if (this.swiper) this.swiper.slidePrev();
+  };
+
+  render() {
+    return (
+      <div>
+        <div className={styles.header}>
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12">
+                <h2 className="title">Testimonials</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <div className={styles.swiper}>
+                <div>
+                  <Swiper
+                    ref={node => {
+                      if (node && node.swiper) {
+                        this.swiper = node.swiper;
+                      }
+                    }}
+                    loop={true}
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                  >
+                    {data.map(testimonial => (
+                      <a className={styles.link} href={testimonial.link} target="_blank" key={testimonial.name}>
+                        <div className={styles.slide}>
+                          <div className={styles.text}>{testimonial.text}</div>
+                          <div className={styles.info}>
+                            <div className={styles.date}>{testimonial.date}</div>
+                            <div className={styles.profile}>
+                              <img src={require(`../../../img/testimonials/${testimonial.image}`)} alt={testimonial.name} />
+                              <h3>{testimonial.name}</h3>
+                            </div>
+                          </div>
+                        </div>
+                      </a>
+                    ))}
+                  </Swiper>
+                </div>
+
+                <div className={`${styles.prev} ${styles.arrow}`} onClick={this.goPrev} />
+                <div className={`${styles.next} ${styles.arrow}`} onClick={this.goNext} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="row">
-        <Testimonial
-          img="/img/testimonials/cryptotalk.jpg"
-          name="Crypto Talk‏"
-          network="twitter"
-          link="https://twitter.com/bitreff/status/907478815890325504"
-          text="Check Out @NexchangeIO with a Simple, Secure and a transparent
-						exchange that you use without registration."
-          date="September 11, 2017, 10:39 PM"
-        />
-        <Testimonial
-          img="/img/testimonials/john.jpg"
-          name="John Michael"
-          network="facebook"
-          link="https://www.facebook.com/rockztar.jm/posts/1475653122471191:0"
-          text="N.exchange is the next big exchange… clean ui, mobile friendly,
-						smooth transaction and transparent platform. I have tried different types
-						exchanges but this platform delivers what I want…"
-          date="September 11, 2017, 01:49 PM"
-        />
-        <Testimonial
-          img="/img/testimonials/tomo.jpg"
-          name="Tomo Adria"
-          network="facebook"
-          link="https://www.facebook.com/permalink.php?story_fbid=1884024031850695&id=100007293495907&substory_index=0"
-          text="Best new instant exchange service! Fast, easy, low fees..."
-          date="September 9, 2017, 22:30 PM"
-        />
-        <Testimonial
-          img="/img/testimonials/hunterk.png"
-          name="Hunterk13"
-          network="bitcointalk"
-          link="https://bitcointalk.org/index.php?topic=2165808.msg21736006#msg21736006"
-          text="I traded $24,000 worth of BTC to LTC yesterday, great trustworthy exchange.
-						Had a few hiccups but  everything was sorted out. Would use again. 😊"
-          date="September 11, 2017, 07:51 PM"
-        />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Testimonials;
