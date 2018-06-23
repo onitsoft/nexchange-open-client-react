e2cac88bf74adc28f9dece94c6d3247b4d3f5831
Karolis Ramanauskas <hello@karolisram.com>
2018-06-23 16:43:00 +0300

    Adjust header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,7 @@ import { fetchCoinDetails, fetchPairs } from 'Actions';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Team from './Team/Team';
+import ICO from './ICO/ICO';
 import Testimonials from './Testimonials/Testimonials';
 import RecentOrders from './RecentOrders/RecentOrders';
 import SubscriptionForm from './SubscriptionForm/SubscriptionForm';
@@ -23,6 +24,7 @@ export class Home extends Component {
       <div>
         <Hero {...this.props} />
         <RecentOrders />
+        <ICO />
         <Testimonials />
         <PriceComparison />
         <About />
