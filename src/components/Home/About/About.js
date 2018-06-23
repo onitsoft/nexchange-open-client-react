61e54dbdd16297c49ee2048dd4aadc2839997318
Karolis Ramanauskas <hello@karolisram.com>
2018-06-23 14:22:00 +0300

    Finish About Us section

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -7,31 +7,37 @@ const About = () => (
       <div className="row">
         <div className="col-xs-12">
           <h2 className="title">About Us</h2>
+        </div>
 
+        <div className="col-xs-12 col-sm-6">
           <p>
             N.exchange is a fast, reliable and fully transparent cryptocurrency exchange built by crypto enthusiasts, for crypto
             enthusiasts.
           </p>
+
           <p>We are a group of multi-national, multi-disciplinary cryptocurrency professionals.</p>
+
           <p>
             Our team came together to make this platform after seeing firsthand the need for a fast and reliable exchange on the crypto
             market.
           </p>
-          <p>
-            We pride ourselves on total transparency: Everything is viewable through our API, starting from order lists and ending with our
-            coin reserves.{' '}
-          </p>
+
+          <p>We pride ourselves on total transparency:</p>
+        </div>
+
+        <div className="col-xs-12 col-sm-6">
+          <p>Everything is viewable through our API, starting from order lists and ending with our coin reserves. </p>
+
           <p>
             N.exchange is built on several core values that guide our team in our day to day operations. Among them are complete
             transparency, a strive to help make cryptocurrency as accessible as possible, and a passion for open source.
           </p>
+
           <p>
             To support our mission of making crypto accessible to everyone, we’ve made our{' '}
-            <b>
-              <a href="https://github.com/onitsoft/nexchange-open-client-react" target="_blank" rel="noopener noreferrer">
-                front end client completely open source
-              </a>
-            </b>.
+            <a href="https://github.com/onitsoft/nexchange-open-client-react" target="_blank" rel="noopener noreferrer">
+              front end client completely open source
+            </a>.
           </p>
         </div>
       </div>
