87cdd324f74c7a1a6c7bbfc75a8d1c43259b3b4e
Karolis Ramanauskas <hello@karolisram.com>
2018-06-28 16:50:11 +0300

    Improve animation perf.

--- src/components/Home/ICO/Ellipse/Ellipse.js
+++ src/components/Home/ICO/Ellipse/Ellipse.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Ellipse.scss';
 
-const Ellipse = props => {
-  return (
-    <div className={`${styles.container} hidden-xs`} style={props.style}>
-      <div className={styles.ellipse}>
-        <img src={require(`Img/ellipses/${props.coin}.svg`)} alt="LTC" />
+class Ellipse extends Component {
+  state = {
+    top: 0,
+    left: 0,
+  };
+
+  componentDidMount = () => {
+    setTimeout(this.updatePos, 0);
+  };
+
+  updatePos = () => {
+    this.setState({
+      top: 200 * (Math.random() > 0.5 ? -1 : 1),
+      left: 200 * (Math.random() > 0.5 ? -1 : 1),
+    });
+
+    setTimeout(this.updatePos, Math.random() * (2500 - 1500) + 1500);
+  };
+
+  render() {
+    return (
+      <div className={`${styles.container} hidden-xs`} style={this.props.style}>
+        <div
+          className={styles.ellipse}
+          style={{
+            width: this.props.style.width,
+            height: this.props.style.height,
+            transform: `translate(${this.state.top}px, ${this.state.left}px)`,
+          }}
+        >
+          <img src={require(`Img/ellipses/${this.props.coin}.svg`)} alt="LTC" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Ellipse;
