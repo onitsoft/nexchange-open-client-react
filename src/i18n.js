04fd1ecb7d455d86a18011a0191a695697fee427
Karolis Ramanauskas <hello@karolisram.com>
2018-07-25 15:32:00 +0300

    Add fallback langauge of English.

--- src/i18n.js
+++ src/i18n.js
@@ -1,40 +1,3 @@
-// import i18n from 'i18next';
-// import Backend from 'i18next-xhr-backend';
-// import { reactI18nextModule } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
-
-// i18n
-//   .use(LanguageDetector)
-//   .use(Backend)
-//   .use(reactI18nextModule)
-//   .init(
-//     {
-//       fallbackLng: 'en',
-//       saveMissing: 'true',
-
-//       // have a common namespace used around the full app
-//       ns: ['translations'],
-//       defaultNS: 'translations',
-
-//       debug: false,
-
-//       interpolation: {
-//         escapeValue: false, // not needed for react!!
-//       },
-
-//       react: {
-//         wait: true,
-//       },
-//     },
-//     (err, t) => {
-//       if (err) {
-//         console.error('error while initialising i18next ::: ' + JSON.stringify(err));
-//       }
-//     }
-//   );
-
-// export default i18n;
-
 import i18n from 'i18next';
 import XHR from 'i18next-xhr-backend';
 import { reactI18nextModule } from 'react-i18next';
@@ -47,15 +10,15 @@ i18n
   .init(
     {
       lng: 'en',
+      fallbackLng: 'en',
 
-      // have a common namespace used around the full app
       ns: ['translations'],
       defaultNS: 'translations',
 
       debug: false,
 
       interpolation: {
-        escapeValue: false, // not needed for react!!
+        escapeValue: false,
       },
 
       saveMissing: true,
