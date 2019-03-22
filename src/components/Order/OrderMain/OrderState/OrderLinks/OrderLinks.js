029fd4207cb19fa435e54d711318c11cecaa437a
Miguel Morujão <miguelmorujao@gmail.com>
2019-03-22 11:56:44 +0000

    Only show "Repeat Order" btn on status Completed & Expired

--- src/components/Order/OrderMain/OrderState/OrderLinks/OrderLinks.js
+++ src/components/Order/OrderMain/OrderState/OrderLinks/OrderLinks.js
@@ -4,11 +4,14 @@ import getBlockchainUrl from 'Utils/getBlockchainUrl';
 import styles from './OrderLinks.scss';
 import { I18n } from 'react-i18next';
 
+import STATUS_CODES from 'StatusCodes';
+
 const OrderLinks = props => (
   <I18n ns="translations">
     {t => (
       <div className={styles.links}>
-        {props.order && (
+      {console.log(props)}
+        {props.order && STATUS_CODES[props.order.status_name[0][0]] === "COMPLETED" && (
           <a href={`/?pair=${props.order.pair.name}&amount_quote=${props.order.amount_quote}`+
           `&withdraw_address=${props.order.withdraw_address.address}`}
           className={`btn ${styles.btn}`} target="_blank">
