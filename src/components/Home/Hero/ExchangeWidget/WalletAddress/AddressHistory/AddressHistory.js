70b08147743396dd6205c16e0ea46ca589b7beda
Miguel Morujão <miguelmorujao@gmail.com>
2019-03-26 11:09:46 +0000

    Translations

--- src/components/Home/Hero/ExchangeWidget/WalletAddress/AddressHistory/AddressHistory.js
+++ src/components/Home/Hero/ExchangeWidget/WalletAddress/AddressHistory/AddressHistory.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { I18n } from 'react-i18next';
 import moment from 'moment';
 import getDaysAgo from 'Utils/getDaysAgo';
 import styles from './AddressHistory.scss';
@@ -11,19 +12,26 @@ class AddressHistory extends Component {
 
   render() {
     return (
-      <div className={`${styles.container}`}>
-        {this.props.history &&
-          this.props.history.map((order, index) => (
-            <div
-            className={`${styles.entry}`} key={index + order.withdraw_address}
-            onMouseDown={() => this.handleClick(order.withdraw_address)}>
-              {order.withdraw_address}
-              <div className={`${styles.details}`}>{`(${order.quote}) ${getDaysAgo(order.created_at)} days ago`}</div>
-            </div>
-          ))
-        }
-        <div className={`${styles.separator}`}></div>
-      </div>
+      <I18n ns="translations">
+        {t => (
+          <div className={`${styles.container}`}>
+            {this.props.history &&
+              this.props.history.map((order, index) => (
+                <div
+                className={`${styles.entry}`} key={index + order.withdraw_address}
+                onMouseDown={() => this.handleClick(order.withdraw_address)}>
+                  {order.withdraw_address}
+                  <div className={`${styles.details}`}>
+                    {`(${order.quote}) ${t('order.history.used')}: ${getDaysAgo(order.created_at) > 0 ?
+                      `${getDaysAgo(order.created_at)} ${t('order.history.daysago')}` : `${t('order.history.today')}`} `}
+                  </div>
+                </div>
+              ))
+            }
+            <div className={`${styles.separator}`}></div>
+          </div>
+        )}
+       </I18n>
     );
   }
 }
