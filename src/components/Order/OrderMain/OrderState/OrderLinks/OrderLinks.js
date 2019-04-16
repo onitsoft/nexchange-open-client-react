7d8a0cc58dfcee2b5bbffadf090036ac4157344d
Miguel Morujão <miguelmorujao@gmail.com>
2019-04-16 02:37:27 +0100

    fixing warnings

--- src/components/Order/OrderMain/OrderState/OrderLinks/OrderLinks.js
+++ src/components/Order/OrderMain/OrderState/OrderLinks/OrderLinks.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import config from 'Config';
 import getBlockchainUrl from 'Utils/getBlockchainUrl';
 import styles from './OrderLinks.scss';
 import { I18n } from 'react-i18next';
@@ -17,12 +16,6 @@ const OrderLinks = props => (
             {t('order.repeat')}
           </a>)
         }
-        {/* TO DELETE
-          props.order && (
-          <a href={`${config.API_BASE_URL}/orders/${props.order.unique_reference}`} className={`btn ${styles.btn}`} target="_blank">
-            {t('order.api')}
-          </a>
-        )*/}
         {props.coin &&
           props.txId && (
             <a href={getBlockchainUrl(props.coin, props.txId)} className={`btn ${styles.btn}`} target="_blank">
