03830dd3532fb35746490bda9c7991167038c7f1
Miguel Morujão <miguelmorujao@gmail.com>
2019-04-16 21:14:35 +0100

    Don't change pair after coin change

--- src/actions/index.js
+++ src/actions/index.js
@@ -275,6 +275,7 @@ export const fetchPairs = () => dispatch => {
             receive: receiveCoin,
           },
           lastSelected: 'deposit',
+          selectedByUser: false
         })
       );
     })
