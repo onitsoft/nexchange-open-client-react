97a62521df5523a17968e63c3b46e672ed97e373
Miguel Morujão <miguelmorujao@gmail.com>
2019-04-18 18:38:02 +0100

    Order Mode change redesign

--- src/components/Home/Hero/OrderModeSwitch/OrderModeSwitch.js
+++ src/components/Home/Hero/OrderModeSwitch/OrderModeSwitch.js
@@ -9,13 +9,16 @@ class OrderModeSwitch extends Component {
     return (
       <I18n ns="translations">
         {t => (
-                <div className={styles.container}>
+
+                <div className={`col-xs-12`}>
+                  <div className={`col-xs-12 col-sm-12 col-md-4 col-lg-3 ${styles.container}`}>
                     <div className={`${styles.basic} ${this.props.orderMode === 'BASIC' ? styles.active : ''}`}>
                         <h3 onClick={() => this.props.changeOrderMode('BASIC')}>Simple</h3>
                     </div>
                     <div className={`${styles.pro} ${this.props.orderMode === 'ORDER_BOOK' ? styles.active : ''}`}>
-                        <h3 onClick={() => this.props.changeOrderMode('ORDER_BOOK')}>Pro</h3>
+                        <h3 onClick={() => this.props.changeOrderMode('ORDER_BOOK')}>Advanced</h3>
                     </div>
+                  </div>
                 </div>
         )}
       </I18n>
