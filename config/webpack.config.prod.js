5d7a9e78f195e1c009dfdcfa1b91f9094c945688
Miguel Morujão <miguelmorujao@gmail.com>
2019-04-22 18:41:34 +0100

    Updated Jest related packages

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -316,7 +316,7 @@ module.exports = {
         minifyURLs: true,
       },
     }),
-    new InterpolateHtmlPlugin(env.raw),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
     // It is absolutely essential that NODE_ENV was set to production here.
