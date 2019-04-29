0afb9e4cae60b22887e5dca1d6fa13665a3c3e3b
Miguel Morujão <miguelmorujao@gmail.com>
2019-04-29 17:37:46 +0100

    + Orderbook tests

--- src/components/Home/Hero/OrderBookWidget/OrderDepth/OrderDepth.test.js
+++ src/components/Home/Hero/OrderBookWidget/OrderDepth/OrderDepth.test.js
@@ -5,33 +5,44 @@ import OrderDepth from './OrderDepth.js';
 describe('OrderDepth', () => {
 
   let wrapShallow;
-  beforeEach(() => {
-    const selectedCoins = {receive: 'DOGE', deposit: 'ETH'};
-    const sellDepth = [
-      { size: 100.000000000, rate: 0.000019500 },
-      { size: 100.000000000, rate: 0.000019400 },
-      { size: 573.000000000, rate: 0.000017510 }    
-      ];
-    const buyDepth = [
-      { size: 2400.000000000, rate: 0.000015000 },
-      { size: 1175.002932550, rate: 0.000017050 },
-      { size: 0.000000400, rate: 55666.000000000 }    
-      ];
+  const selectedCoins = {receive: 'DOGE', deposit: 'ETH'};
+  const sellDepth = [
+    { size: 100.000000000, rate: 0.000019500 },
+    { size: 100.000000000, rate: 0.000019400 },
+    { size: 573.000000000, rate: 0.000017510 }    
+    ];
+  const buyDepth = [
+    { size: 2400.000000000, rate: 0.000015000 },
+    { size: 1175.002932550, rate: 0.000017050 },
+    { size: 0.000000400, rate: 55666.000000000 }    
+    ];
 
+  beforeEach(() => {
     wrapShallow = shallow(<OrderDepth 
                             selectedCoins={selectedCoins} 
                             sellDepth={sellDepth}
-                            buyDepth={buyDepth} />);
+                            buyDepth={buyDepth} />).dive();
                     });
 
   it('renders correctly', () => {
     expect(wrapShallow).toMatchSnapshot();
   });
 
-  it('contains', () => {
-
+  it('contains 3 sell depth items and 3 buy depth items', () => {
+    expect(wrapShallow.find('[data-test="sell-depth-item"]').length).toBe(3);
+    expect(wrapShallow.find('[data-test="buy-depth-item"]').length).toBe(3);
   });
 
-  it('', () => {
+  it('has correct spread value', () => {
+    let spreadValue;
+    const bestBid = sellDepth[sellDepth.length -1];
+    const bestAsk = buyDepth[0];
+
+    spreadValue = (bestAsk.rate - bestBid.rate) / ((bestAsk.rate + bestBid.rate)/2) * 100;
+    spreadValue = parseFloat(Math.round(spreadValue * 100) / 100).toFixed(2);
+    console.log(wrapShallow.find('[data-test="spread"]').render().text());
+
+    expect(wrapShallow.find('[data-test="spread"]').text()).toEqual(`Spread ${spreadValue}%`);
   });
+
 });
