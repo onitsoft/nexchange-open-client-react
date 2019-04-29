2c231b79f5db2239b459fb6433b587116bd99966
Miguel Morujão <miguelmorujao@gmail.com>
2019-04-29 11:33:07 +0100

    Added OrderModeSwitch tests

--- src/components/Home/Hero/OrderModeSwitch/OrderModeSwitch.js
+++ src/components/Home/Hero/OrderModeSwitch/OrderModeSwitch.js
@@ -12,14 +12,14 @@ class OrderModeSwitch extends Component {
 
                 <div className={`col-xs-12`}>
                   <div className={`col-xs-12 col-sm-12 col-md-4 col-lg-3 ${styles.container}`}>
-                    <div className={`${styles.basic} ${this.props.orderMode === 'BASIC' ? styles.active : ''}`}
-                      onClick={() => this.props.changeOrderMode('BASIC')}>
+                    <a className={`${styles.basic} ${this.props.orderMode === 'BASIC' ? styles.active : ''}`}
+                      onClick={() => this.props.changeOrderMode('BASIC')} data-test='BASIC'>
                         <h3>Simple</h3>
-                    </div>
-                    <div className={`${styles.pro} ${this.props.orderMode === 'ORDER_BOOK' ? styles.active : ''}`}
-                      onClick={() => this.props.changeOrderMode('ORDER_BOOK')}>
+                    </a>
+                    <a className={`${styles.pro} ${this.props.orderMode === 'ORDER_BOOK' ? styles.active : ''}`}
+                      onClick={() => this.props.changeOrderMode('ORDER_BOOK')} data-test='ORDER_BOOK'>
                         <h3>Advanced</h3>
-                    </div>
+                    </a>
                   </div>
                 </div>
         )}
