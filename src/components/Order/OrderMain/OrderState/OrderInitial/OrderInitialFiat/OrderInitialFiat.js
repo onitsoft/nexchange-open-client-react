19f1d6207089d8391ac270d21341dc5187bc326a
Miguel Morujão <miguelmorujao@gmail.com>
2019-05-04 16:51:48 +0100

    fix

--- src/components/Order/OrderMain/OrderState/OrderInitial/OrderInitialFiat/OrderInitialFiat.js
+++ src/components/Order/OrderMain/OrderState/OrderInitial/OrderInitialFiat/OrderInitialFiat.js
@@ -125,22 +125,24 @@ const removeUnnecessaryURLParams = (url) => {
 } 
 
 const removeURLParam = (url, parameter) => {
-  //prefer to use l.search if you have a location/link object
-  var urlparts = url.split('?');   
-  if (urlparts.length >= 2) {
+  if(!_.isEmpty(url) && !_.isEmpty(param)) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');   
+    if (urlparts.length >= 2) {
 
-      var prefix = encodeURIComponent(parameter) + '=';
-      var pars = urlparts[1].split(/[&;]/g);
+        var prefix = encodeURIComponent(parameter) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
 
-      //reverse iteration as may be destructive
-      for (var i = pars.length; i-- > 0;) {    
-          //idiom for string.startsWith
-          if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
-              pars.splice(i, 1);
-          }
-      }
+        //reverse iteration as may be destructive
+        for (var i = pars.length; i-- > 0;) {    
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
+                pars.splice(i, 1);
+            }
+        }
 
-      return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
   }
   return url;
 }
