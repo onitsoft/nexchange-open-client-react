2acfb5ed4a276af195d213f75eba58c642b3723c
Miguel Morujão <miguelmorujao@gmail.com>
2019-05-06 08:52:37 +0100

    Build fix

--- .storybook/config/webpack.config.prod.js
+++ .storybook/config/webpack.config.prod.js
@@ -263,7 +263,6 @@ module.exports = {
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     // In production, it will be an empty string unless you specify "homepage"
     // in `package.json`, in which case it will be the pathname of that URL.
-    new InterpolateHtmlPlugin(env.raw),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
@@ -281,6 +280,7 @@ module.exports = {
         minifyURLs: true,
       },
     }),
+    new InterpolateHtmlPlugin(env.raw),
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
     // It is absolutely essential that NODE_ENV was set to production here.
