ec6a820e9aa4c508ec5d19ecbf4f996f1a6306fe
Miguel Morujão <miguelmorujao@gmail.com>
2019-05-07 09:49:56 +0100

    destination tag fix

--- src/components/Home/Hero/ExchangeWidget/ExchangeWidget.js
+++ src/components/Home/Hero/ExchangeWidget/ExchangeWidget.js
@@ -66,10 +66,13 @@ class ExchangeWidget extends Component {
         address: this.props.wallet.address,
         name: '',
         payment_id: this.props.paymentId.paymentId,
-        destinationTag: this.props.destinationTag.destinationTag,
+        destination_tag: this.props.destinationTag.destinationTag,
         memo: this.props.memo.memo,
       },
     };
+
+    console.log(data);
+  
 
     if (this.props.price.lastEdited === 'receive') data['amount_base'] = parseFloat(this.props.price.receive);
     else if (this.props.price.lastEdited === 'deposit') data['amount_quote'] = parseFloat(this.props.price.deposit);
