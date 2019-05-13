73b9aeaa541883f7bc7af98c8704899466192d1c
Miguel Morujão <miguelmorujao@gmail.com>
2019-05-14 00:41:00 +0100

    Don't hide switch button when pair is not supported

--- src/components/Home/Hero/ExchangeWidget/CoinSwitch/CoinSwitch.js
+++ src/components/Home/Hero/ExchangeWidget/CoinSwitch/CoinSwitch.js
@@ -33,18 +33,20 @@ class CoinSwitch extends Component {
   };
 
   render() {
-    let showSwitchButton = false;
+    let switchButtonEnabled = false;
     const nextDeposit = this.props.selectedCoin.receive;
     const nextReceive = this.props.selectedCoin.deposit;
 
     if(this.props.pairs && this.props.pairs[nextDeposit] && this.props.pairs[nextDeposit][nextReceive]){
-      showSwitchButton = true;
+      switchButtonEnabled = true;
     }
 
     return (
       <div className={`col-xs-12 col-sm-1 ${styles.container}`}>
-        { showSwitchButton ? <span className={`clickable ${styles.icon}`} onClick={this.handleClick}></span>
-        : null }
+      <span 
+        className={`${styles.icon} ${!switchButtonEnabled ? styles['icon-disabled'] : ''}`} 
+        onClick={switchButtonEnabled ? this.handleClick : null}>
+      </span>
       </div>
     );
   }
