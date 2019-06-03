60a0097dd9f14454c30edc7cbe8eb08c24bc7c42
Miguel Morujão <miguelmorujao@gmail.com>
2019-06-03 10:21:54 +0100

    fix

--- src/components/Home/Hero/Integrations/Metamask/Metamask.js
+++ src/components/Home/Hero/Integrations/Metamask/Metamask.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { open } from '@colony/purser-metamask'
-import { setWallet, errorAlert } from 'Actions/index.js';
+import { setWallet, errorAlert, selectCoin, fetchPrice } from 'Actions/index.js';
 import i18n from 'Src/i18n';
 
 import styles from '../Integrations.scss';
@@ -16,8 +16,28 @@ class Metamask extends Component {
             address: wallet.address,
             valid: true,
             });
+
+            console.log(this);
+            const depositCoin = this.props.selectedCoin.deposit;
+            const receiveCoin = 'ETH';
+
+            this.props.selectCoin({
+              ...this.props.selectedCoin,
+              receive: receiveCoin
+            }, this.props.pairs);
+      
+            const pair = `${receiveCoin}${depositCoin}`;
+            const data = {
+              pair,
+              lastEdited: 'receive',
+            };
+      
+            data['deposit'] = receiveCoin;
+            data['receive'] = depositCoin;
+            this.props.fetchPrice(data);
         }
     } catch (error) {
+        console.log(error); 
         this.props.errorAlert({
             show: true,
             message: i18n.t('integrations.error.metamask'),
@@ -35,8 +55,8 @@ class Metamask extends Component {
   }
 }
 
-const mapStateToProps = () => ({});
-const mapDispatchToProps = dispatch => bindActionCreators({ setWallet, errorAlert }, dispatch);
+const mapStateToProps = ({ selectedCoin, pairs }) => ({ selectedCoin, pairs });
+const mapDispatchToProps = dispatch => bindActionCreators({ setWallet, errorAlert, selectCoin, fetchPrice }, dispatch);
 
 export default connect(
     mapStateToProps,
