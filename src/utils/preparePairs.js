90bac2d4c373ff222da02fdb6df4187f130e83d9
Miguel Morujão <miguelmorujao@gmail.com>
2019-06-06 16:50:24 +0100

    Dragondex

--- src/utils/preparePairs.js
+++ src/utils/preparePairs.js
@@ -1,11 +1,14 @@
 export default data => {
   let pairs = {};
   for (let pair of data) {
+    console.log(pair);
     if (!pair.disabled) {
       if (!pairs[pair.quote]) pairs[pair.quote] = {};
-      pairs[pair.quote][pair.base] = !pair.disabled; // pair[deposit][receive]
+      pairs[pair.quote][pair.base] = {};
+      pairs[pair.quote][pair.base].enabled = !pair.disabled; 
+      pairs[pair.quote][pair.base].orderbook_enabled = pair.orderbook_enabled;
+      pairs[pair.quote][pair.base].reverse_orderbook_enabled = pair.reverse_orderbook_enabled;
     }
   }
-
   return pairs;
 };
