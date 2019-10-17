e5c6f37c6b7c6241870f67bec62bd4c5bc575f49
duncan <dun.can@tuta.io>
2019-10-17 19:56:31 +0300

    1. Improve SupportedAssets component; structure. 2. Add SupportedAsset child component.

---
+++ src/components/WhiteLabel/SupportedAssets/SupportedAsset/SupportedAsset.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './SupportedAsset.scss';
+
+
+function SupportedAsset( { name } ) {
+
+  const supportedAssetImageLocation = `/img/supported_asset/${name}.jpg`
+
+  return (
+    <React.Fragment>
+      <div className={styles.profile}>
+        <img src={supportedAssetImageLocation} alt={`${name}`} />
+      </div>
+      <div className={styles.title}>
+        {name}
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default SupportedAsset
--- src/components/WhiteLabel/SupportedAssets/SupportedAssets.js
+++ src/components/WhiteLabel/SupportedAssets/SupportedAssets.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './SupportedAssets.scss';
 import { I18n, Trans } from 'react-i18next';
+import SupportedAsset from './SupportedAsset/SupportedAsset'
 
 
 function SupportedAssets() {
-    return (
-        <p>SupportedAssets</p>
-    )
+  return (
+    <React.Fragment>
+      <div className="col-md-12">
+        <h2>Supported Digital Assets</h2>
+      </div>
+      <div className="col-md-2">
+        <SupportedAsset />
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default SupportedAssets
