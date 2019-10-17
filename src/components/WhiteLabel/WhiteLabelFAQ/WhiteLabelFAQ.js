4482f51872f9510dbf98dc9bbd2f1ab0f7dba93f
duncan <dun.can@tuta.io>
2019-10-17 20:01:49 +0300

    Write WhiteLabelFAQ component, complaint with Google FAQ Microdata format.

--- src/components/WhiteLabel/WhiteLabelFAQ/WhiteLabelFAQ.js
+++ src/components/WhiteLabel/WhiteLabelFAQ/WhiteLabelFAQ.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import styles from './WhiteLabelFAQ.scss';
-import { I18n, Trans } from 'react-i18next';
+import React from 'react'
+import { Col } from 'reactstrap'
+import { Generic, GenericCollection, JSONLD } from 'react-structured-data'
 
 
 function WhiteLabelFAQ() {
-    return (
-        <p>WhiteLabelFAQ</p>
-    )
+
+  const faqs = [
+    {
+      question: 'Some question?',
+      answer: 'Some answer',
+      id: 1
+    },
+    {
+      question: 'Another Q?',
+      answer: 'Another A',
+      id: 2
+    }
+  ]
+
+  // Add from Helmet example, https://github.com/etereo-io/react-seo-bootstrap/blob/9cf8f14411ee9f2ab1bbc3574c310c188bed3c63/src/components/faqs/Faqs.js#L22
+  return (
+    <Col md={12}>
+      <JSONLD>
+        <Generic type="FAQPage" jsonldtype="FAQPage">
+          <GenericCollection type="mainEntity">
+            {faqs.map(faq => (
+              <Generic
+                key={ faq.id }
+                jsonldtype="Question"
+                schema={{ name: faq.question }}
+              >
+                <Generic
+                  type="acceptedAnswer"
+                  jsonldtype="Answer"
+                  schema={{ type: faq.answer }}
+                />
+              </Generic>
+            ))}
+          </GenericCollection>
+        </Generic>
+      </JSONLD>
+
+      <div className="faq-list">
+        { faqs.map(faq => (
+          <div className="faq" key={faq.id}>
+            <h3>{ faq.question }</h3>
+            <p className="answer">{ faq.answer }</p>
+          </div>
+        ))}
+      </div>
+    </Col>
+  )
 }
 
 export default WhiteLabelFAQ
