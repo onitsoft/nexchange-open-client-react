6fca56112610c61c8fb1ed778035169cecd8a1a9
duncan <dun.can@tuta.io>
2019-10-17 19:54:01 +0300

    Improve MajorCard component; structure.

--- src/components/WhiteLabel/MajorCard/MajorCard.js
+++ src/components/WhiteLabel/MajorCard/MajorCard.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import styles from './MajorCard.scss';
-import { I18n, Trans } from 'react-i18next';
+import React from 'react'
+import { Col } from 'reactstrap'
+import styles from './MajorCard.scss'
+import { reactI18nextModule } from 'react-i18next/dist/commonjs/context'
+
 
 function MajorCard() {
-    return (
-        <p>MajorCard</p>
-    )
+  return (
+    <React.Fragment>
+      <Col md={12}>
+        <h2>Turn-Key Solution to Start Your Own Instant Exchange</h2>
+      </Col>
+      <Col md={6}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nisi nec nunc euismod condimentum.</p>
+        <p>Aenean efficitur metus vel posuere hendrerit. </p>
+        <p>Sed ut arcu vel velit tempor fermentum vel varius dui. Duis lacus dolor, euismod id.</p>
+      </Col>
+      <Col md={6}>
+        <p>Image</p>
+      </Col>
+    </React.Fragment>
+  )
 }
 
 export default MajorCard
--- src/components/WhiteLabel/MinorCard/MinorCard.js
+++ src/components/WhiteLabel/MinorCard/MinorCard.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import styles from './MinorCard.scss';
-import { I18n, Trans } from 'react-i18next';
+import React from 'react'
+import { Col } from 'reactstrap'
+import styles from './MinorCard.scss'
+import { I18n, Trans } from 'react-i18next'
 
 
-function MinorCard() {
-    return (
-        <p>MinorCard</p>
-    )
+function MinorCard({ topic }) {
+
+  const featureImageLocation = `/img/white_label_page/feature/${topic}.jpg`
+
+  return (
+    <React.Fragment>
+      <Col md={6}>
+        <h2>{topic}</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nisi nec nunc euismod condimentum.</p>
+        <p>Aenean efficitur metus vel posuere hendrerit. </p>
+        <p>Sed ut arcu vel velit tempor fermentum vel varius dui. Duis lacus dolor, euismod id.</p>
+      </Col>
+      <Col md={6}>
+        <p>{featureImageLocation}</p>
+      </Col>
+    </React.Fragment>
+  )
 }
 
 export default MinorCard
