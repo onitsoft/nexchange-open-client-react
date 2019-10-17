d02c7220798d0189bedf75c2e60cdb258a45580a
duncan <dun.can@tuta.io>
2019-10-17 19:53:05 +0300

    Improve KeyFeatures component; structure.

--- src/components/WhiteLabel/KeyFeatures/KeyFeatures.js
+++ src/components/WhiteLabel/KeyFeatures/KeyFeatures.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import styles from './KeyFeatures.scss';
-import { I18n } from 'react-i18next';
+import React from 'react'
+import { Col } from 'reactstrap'
+import styles from './KeyFeatures.scss'
+import { I18n } from 'react-i18next'
+
+import FeatureItem from './FeatureItem/FeatureItem'
 
 
 function KeyFeatures() {
 
+  // const features = [
+  //   { name: 'security', description: 'top' },
+  //   { another... },
+  //   { another... }
+
+  // map over features array passing in values via props
   return (
     <I18n ns="translations">
       {t => (
-        <div className={styles.container}>
-          <p>KeyFeatures</p>
-        </div>
+        <React.Fragment>
+          <Col md={12}>
+            <h2>Key Features of Instant Exchange</h2>
+          </Col>
+          <Col md={4}>
+            < FeatureItem name='Sec' description='Top' />
+          </Col>
+        </React.Fragment>
       )}
     </I18n>
   )
