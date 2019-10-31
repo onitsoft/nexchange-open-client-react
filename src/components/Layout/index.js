9fefdebd85d5a3261f781654832e8b6b25b63fd8
Duncan Betts <betts.duncan@tuta.io>
2019-10-31 14:21:27 +0000

    Start implementing grid layout.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,23 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 import Referrals from 'Components/Layout/Referrals/Referrals'
 import Header from 'Components/Layout/Header/Header'
 import Footer from 'Components/Layout/Footer/Footer'
 
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: grid;
+`
+
 
 export default function Layout({ children }) {
     return (
-        <Fragment>
+        <Wrapper>
             <Referrals />
             <Header />
               { children }
             <Footer />
-        </Fragment>
+        </Wrapper>
     )
 }
