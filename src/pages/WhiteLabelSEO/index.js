83a35d695297aea9c7a040a37228074a692ec515
Eli Sklar <eli.sklar@gmail.com>
2019-11-04 15:52:27 +0200

    add in new FAQ comp

--- src/pages/WhiteLabelSEO/index.js
+++ src/pages/WhiteLabelSEO/index.js
@@ -7,7 +7,7 @@ import KeyFeatures from 'Components/WhiteLabel/KeyFeatures/'
 import MajorCard from 'Components/WhiteLabel/MajorCard/'
 import SupportedAssets from 'Components/WhiteLabel/SupportedAssets/'
 import MinorCard from 'Components/WhiteLabel/MinorCard/'
-import FAQ from 'Components/WhiteLabel/FAQ/'
+import FAQ from 'Components/WhiteLabel/FAQ2/'
 import PriceTable from 'Components/WhiteLabel/PriceTable/'
 
 import styles from './styles.css'
