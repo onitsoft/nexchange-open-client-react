80c59b7079ee54c9c95d2426e1da7d083d25784f
Eli Sklar <eli.sklar@gmail.com>
2019-11-11 10:41:29 +0200

    fix hero tests

--- src/components/Pair/Hero/Hero.test.js
+++ src/components/Pair/Hero/Hero.test.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Hero from './Hero';
 
+
+const setup = ({
+  selectedCoin = {
+    deposit: 'eth',
+    receive: 'usd'
+  }
+}) => ({
+  selectedCoin
+})
 describe('Hero', () => {
   it('renders correctly', () => {
-    const wrapShallow = shallow(<Hero />).dive();
+    const props = setup({})
+    const wrapShallow = shallow(<Hero {...props} />).dive();
     expect(wrapShallow).toMatchSnapshot();
   });
 });
