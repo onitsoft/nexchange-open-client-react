d52ab974d2d485716b5b142840b4313208fb4ce8
Eli Sklar <eli.sklar@gmail.com>
2019-11-18 18:19:08 +0200

    change order of links in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -27,8 +27,8 @@ const Footer = (props) => {
               <section>
                 <h4>{t('header.resources')}</h4>
                 <ul>
-                  <li><a href='https://nexchange2.docs.apiary.io/'>{t('header.apidocumentation')}</a></li>
                   <li><Link to='/instant-white-label'>{t('header.whitelabel')}</Link></li>
+                  <li><a href='https://nexchange2.docs.apiary.io/'>{t('header.apidocumentation')}</a></li>
                   <li><a href='/#support'>{t('header.support')}</a></li>
                 </ul>
               </section>
