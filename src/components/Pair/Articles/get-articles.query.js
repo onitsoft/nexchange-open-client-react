f96ad89e43018497aec1556dceed4cc12cf0ecba
Eli Sklar <eli.sklar@gmail.com>
2019-11-19 15:00:59 +0200

    styling and articles for coins

--- src/components/Pair/Articles/get-articles.query.js
+++ src/components/Pair/Articles/get-articles.query.js
@@ -1,9 +1,26 @@
 import gql from 'graphql-tag'
 
 export const GET_ARTILCES_QUERY = gql`
-  query GetArticles ($pagename: String) {
-    pages (where: {name: $pagename}) {
-      articles {
+  query GetArticles ($baseName: String, $quoteName: String) {
+    base: page (where: {name: $baseName}) {
+      title
+      content
+      createdAt
+      updatedAt
+      articles (orderBy: sort_ASC) {
+        title
+        content
+        date
+        createdAt
+        updatedAt
+      }
+    }
+    quote: page (where: {name: $quoteName}) {
+      title
+      content
+      createdAt
+      updatedAt
+      articles (orderBy: sort_ASC) {
         title
         content
         date
