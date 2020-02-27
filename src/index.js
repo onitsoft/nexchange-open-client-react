990ca7611d1d8904a22a689ecb6e9fdbe916d347
Integral Team <repo@sfxdx.ru>
2020-02-27 12:30:17 +0700

    added chunk names for debugging

--- src/index.js
+++ src/index.js
@@ -10,19 +10,18 @@ import i18n from 'i18next';
 import setAuthToken from 'Utils/setAuthToken';
 import crispEmailBinding from 'Utils/crispEmailBinding';
 
-import ToTop from 'Components/misc/ToTop'
+import ToTop from 'Components/misc/ToTop';
 
 import reducers from './reducers';
 
-import { BreakpointProvider, defaultQuery } from 'Components/misc/breakpoint'
+import { BreakpointProvider, defaultQuery } from 'Components/misc/breakpoint';
 
-import GraphCMSProvider from './services/graphcms'
-import Intercom from './services/intercom'
+import GraphCMSProvider from './services/graphcms';
+import Intercom from './services/intercom';
 import './css/index.scss';
 
 window.$ = window.jQuery = require('jquery');
 require('./js/bootstrap.min.js');
-
 
 // const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 // const store = createStoreWithMiddleware(reducers);
@@ -35,30 +34,34 @@ setAuthToken();
 crispEmailBinding(store);
 require('Utils/bindGa');
 
-const Referrals = React.lazy(() => import('Components/Referrals/Referrals'))
-const Header = React.lazy(() => import('Components/Header/Header'))
-const Footer = React.lazy(() => import('Components/Footer'))
-const NotFound = React.lazy(() => import('Components/NotFound/NotFound'))
-const FAQ = React.lazy(() => import('Components/FAQ2/FAQ'))
-const Pair = React.lazy(() => import('Components/Pair/Pair'))
+const Referrals = React.lazy(() => import(/* webpackChunkName: "Referrals" */ 'Components/Referrals/Referrals'));
+const Header = React.lazy(() => import(/* webpackChunkName: "Header" */ 'Components/Header/Header'));
+const Footer = React.lazy(() => import(/* webpackChunkName: "Footer" */ 'Components/Footer'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ 'Components/NotFound/NotFound'));
+const FAQ = React.lazy(() => import(/* webpackChunkName: "FAQ" */ 'Components/FAQ2/FAQ'));
+const Pair = React.lazy(() => import(/* webpackChunkName: "Pair" */ 'Components/Pair/Pair'));
 
-const Home = React.lazy(() => import('Components/Home/Home'))
-const TermsConditions = React.lazy(() => import('Components/TermsConditions/TermsConditions'))
-const Privacy = React.lazy(() => import('Components/Privacy/Privacy'))
-const SignIn = React.lazy(() => import('Components/Accounts/SignIn/SignIn'))
-const SignOut = React.lazy(() => import('Components/Accounts/SignOut/SignOut'))
-const SignUp = React.lazy(() => import('Components/Accounts/SignUp/SignUp'))
-const Orders = React.lazy(() => import('Components/Accounts/Orders'))
-const ForgotPassword = React.lazy(() => import('Components/Accounts/ForgotPassword/ForgotPassword'))
-const Order = React.lazy(() => import('Components/Order/Order'))
-const WhiteLabelSEO = React.lazy(() => import('Pages/WhiteLabelSEO'))
-const Profile = React.lazy(() => import('Pages/Profile'))
-const PriceComparison = React.lazy(() => import('Components/Home/PriceComparison/PriceComparison'))
-const Team = React.lazy(() => import('Components/Home/Team/Team'))
-const About = React.lazy(() => import('Components/Home/About/About'))
-const NotFoundRedirect = () => <Redirect to={`/${i18n.language}/not-found`} />
+const Home = React.lazy(() => import(/* webpackChunkName: "Home" */ 'Components/Home/Home'));
+const TermsConditions = React.lazy(() => import(/* webpackChunkName: "TermsConditions" */ 'Components/TermsConditions/TermsConditions'));
+const Privacy = React.lazy(() => import(/* webpackChunkName: "Privacy" */ 'Components/Privacy/Privacy'));
+const SignIn = React.lazy(() => import(/* webpackChunkName: "SignIn" */ 'Components/Accounts/SignIn/SignIn'));
+const SignOut = React.lazy(() => import(/* webpackChunkName: "SignOut" */ 'Components/Accounts/SignOut/SignOut'));
+const SignUp = React.lazy(() => import(/* webpackChunkName: "SignUp" */ 'Components/Accounts/SignUp/SignUp'));
+const Orders = React.lazy(() => import(/* webpackChunkName: "Orders" */ 'Components/Accounts/Orders'));
+const ForgotPassword = React.lazy(() =>
+  import(/* webpackChunkName: "ForgotPassword" */ 'Components/Accounts/ForgotPassword/ForgotPassword')
+);
+const Order = React.lazy(() => import(/* webpackChunkName: "Order" */ 'Components/Order/Order'));
+const WhiteLabelSEO = React.lazy(() => import(/* webpackChunkName: "WhiteLabelSEO" */ 'Pages/WhiteLabelSEO'));
+const Profile = React.lazy(() => import(/* webpackChunkName: "Profile" */ 'Pages/Profile'));
+const PriceComparison = React.lazy(() =>
+  import(/* webpackChunkName: "PriceComparison" */ 'Components/Home/PriceComparison/PriceComparison')
+);
+const Team = React.lazy(() => import(/* webpackChunkName: "Team" */ 'Components/Home/Team/Team'));
+const About = React.lazy(() => import(/* webpackChunkName: "About" */ 'Components/Home/About/About'));
+const NotFoundRedirect = () => <Redirect to={`/${i18n.language}/not-found`} />;
 
-ReactDOM.render((
+ReactDOM.render(
   <GraphCMSProvider>
     <Provider store={store}>
       <BreakpointProvider queries={defaultQuery}>
@@ -69,23 +72,20 @@ ReactDOM.render((
               <Header />
 
               <Switch>
+                <Redirect exact from="/" to="/en" />
                 <Route exact path="/:lang/terms-and-conditions" component={TermsConditions} />
                 <Route exact path="/:lang/privacy" component={Privacy} />
                 <Route exact path="/:lang/profile/:user?" component={Profile} />
                 <Route exact path="/:lang/order/:orderRef" component={Order} />
                 <Route exact path="/:lang/orders/:orderRef?" component={Orders} />
-                <Route exact path="/:lang"
-                  render={props =>  <Home {...props} store={store} />}
-                />
-                <Redirect exact from='/' to="/en" />
+                <Route exact path="/:lang" render={props => <Home {...props} store={store} />} />
                 <Route exact path="/:lang/instant-white-label/" component={WhiteLabelSEO} />
                 <Route exact path="/:lang/faqs/:id?" component={FAQ} />
                 <Route exact path="/:lang/signin" component={SignIn} />
                 <Route exact path="/:lang/signout" component={SignOut} />
                 <Route exact path="/:lang/signup" component={SignUp} />
                 <Route exact path="/:lang/forgot-password/:resetToken?" component={ForgotPassword} />
-                <Route exact path="/:lang/convert/:quote-to-:base"
-                  render={props =>  <Pair {...props} store={store} />} />
+                <Route exact path="/:lang/convert/:quote-to-:base" render={props => <Pair {...props} store={store} />} />
                 <Route exact path="/:lang/not-found" component={NotFound} />
                 <Route component={NotFoundRedirect} />
               </Switch>
@@ -97,7 +97,6 @@ ReactDOM.render((
         </BrowserRouter>
       </BreakpointProvider>
     </Provider>
-  </GraphCMSProvider>
-  ),
+  </GraphCMSProvider>,
   document.getElementById('root')
 );
