2b4e3bfb2dc0cc2e7dccd5588f7a1d06140a6b21
punit2502 <punit.makwana25@gmail.com>
2020-05-19 19:43:15 +0530

    - show error if payment_url is null - autocheck tnc - if terms agreed, dont show it next time

--- src/components/Order/OrderMain/OrderState/OrderInitial/Checkbox/Checkbox.js
+++ src/components/Order/OrderMain/OrderState/OrderInitial/Checkbox/Checkbox.js
@@ -4,42 +4,45 @@ import { I18n } from 'react-i18next';
 class Checkbox extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-     
-    }
+    this.state = {};
   }
 
-  render(){
+  render() {
     const props = this.props;
-    
+
     return (
-        <div>
-            <I18n ns="translations">
-            {(t) => (
+      <div>
+        <I18n ns="translations">
+          {t => (
             <label>
               {/* eslint max-len: ["error", { "code": 200 }] */}
-              <input type="checkbox" name={props.name} id={props.name} value="check" style={{ width: '20px', height: '20px', cursor: 'pointer'}}
-              onClick={function togglePayNowButton() {
-                  let _checkoutButton = document.getElementsByName("checkoutButton")[0];
+              <input
+                type="checkbox"
+                name={props.name}
+                id={props.name}
+                value="check"
+                style={{ width: '20px', height: '20px', cursor: 'pointer' }}
+                onClick={function togglePayNowButton() {
+                  let _checkoutButton = document.getElementsByName('checkoutButton')[0];
                   let _box = document.getElementsByName('checkboxTC')[0];
-                  let _box_kyc = document.getElementsByName("checkboxKYC")[0];
+                  let _box_kyc = document.getElementsByName('checkboxKYC')[0];
                   if (_box.checked && _box_kyc.checked) {
-                      props.onTogglePayment(true)
-                      _checkoutButton.classList.remove("disabled");
+                    props.onTogglePayment(true);
+                    _checkoutButton.classList.remove('disabled');
                   } else {
-                      props.onTogglePayment(false)
-                      _checkoutButton.classList.add("disabled");
+                    props.onTogglePayment(false);
+                    _checkoutButton.classList.add('disabled');
                   }
-              }}/>
-              <strong style={{paddingLeft: "7px", cursor: 'pointer'}} dangerouslySetInnerHTML={{__html: t(props.order)}}/>
+                }}
+                defaultChecked={true}
+              />
+              <strong style={{ paddingLeft: '7px', cursor: 'pointer' }} dangerouslySetInnerHTML={{ __html: t(props.order) }} />
             </label>
-            )}
-            </I18n>
-            
-        </div> 
-       );
-    }
-};
-
+          )}
+        </I18n>
+      </div>
+    );
+  }
+}
 
 export default Checkbox;
