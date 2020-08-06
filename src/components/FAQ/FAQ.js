e655596a79366f1dda87740cd60886a90ace867e
= <=>
2020-08-06 21:11:24 +0600

    added by now

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -228,7 +228,7 @@ class FAQ extends Component {
                                 <li>
                                   <b>Enter</b> your desired <b>amount, choose</b> the currency type you
                                   <b>want to spend</b>, and the currency type you <b>want to buy</b> (e.g. if you want to spend 1 BTC to buy
-                                  ETH at the exchange rate shown, you enter “1” and click on “CONFIRM and PLACE ORDER”)
+                                  ETH at the exchange rate shown, you enter “1” and click on “BUY NOW”)
                                   <img src="/img/faq/step1.png" alt="How does it work, step 1" />
                                 </li>
                                 <li>
