35bd735df3180cbf2588ec502f241f78b9fcfdc5
Joris Medeisis <joris@nexchange.co.uk>
2021-06-25 07:41:20 +0300

    Blog redirect
    * refactor to exact path

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ const App = () => {
                   <Header />
 
                   <Switch>
-                    <Route path="/blog(/*)" render={redirectToBlog}/>
+                    <Route exact path="/blog" render={redirectToBlog}/>
                     <Route exact path="/" render={props => <Redirect to={`/${lang}${props.location.search}`} />} />
                     <Route exact path="/:lang(en|de|ru)/terms-and-conditions" component={TermsConditions} />
                     <Route exact path="/:lang(en|de|ru)/privacy" component={Privacy} />
