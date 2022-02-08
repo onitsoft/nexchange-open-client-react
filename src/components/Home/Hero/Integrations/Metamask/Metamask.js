946bd8d25c3f74895d791dc6fd08b87c1b3f95ef
Joris Medeisis <joris@nexchange.co.uk>
2022-02-08 14:41:30 +0200

    * migrate node 10 -> 14 * disable Metamask integration (caused crash with new node)

--- src/components/Home/Hero/Integrations/Metamask/Metamask.js
+++ src/components/Home/Hero/Integrations/Metamask/Metamask.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { open } from '@colony/purser-metamask'
+// import { open } from '@colony/purser-metamask'
+
 import { setWallet, errorAlert, selectCoin, fetchPrice } from 'Actions/index.js';
 import i18n from 'Src/i18n';
 
@@ -10,32 +11,34 @@ import styles from '../Integrations.scss';
 class Metamask extends Component {
   onClick = async () => {
     try {
-        const wallet = await open();
-        if(wallet && wallet.address) {
-            this.props.setWallet({
-            address: wallet.address,
-            valid: true,
-            });
-
-            const depositCoin = this.props.selectedCoin.deposit;
-            const receiveCoin = 'ETH';
-
-            this.props.selectCoin({
-              ...this.props.selectedCoin,
-              receive: receiveCoin,
-              orderByAddress: true,
-            }, this.props.pairs);
-      
-            const pair = `${receiveCoin}${depositCoin}`;
-            const data = {
-              pair,
-              lastEdited: 'receive',
-            };
-      
-            data['deposit'] = receiveCoin;
-            data['receive'] = depositCoin;
-            this.props.fetchPrice(data);
-        }
+      throw new Error('No metamask integration');
+        // const wallet = await open();
+        //
+        // if(wallet && wallet.address) {
+        //     this.props.setWallet({
+        //     address: wallet.address,
+        //     valid: true,
+        //     });
+        //
+        //     const depositCoin = this.props.selectedCoin.deposit;
+        //     const receiveCoin = 'ETH';
+        //
+        //     this.props.selectCoin({
+        //       ...this.props.selectedCoin,
+        //       receive: receiveCoin,
+        //       orderByAddress: true,
+        //     }, this.props.pairs);
+        //
+        //     const pair = `${receiveCoin}${depositCoin}`;
+        //     const data = {
+        //       pair,
+        //       lastEdited: 'receive',
+        //     };
+        //
+        //     data['deposit'] = receiveCoin;
+        //     data['receive'] = depositCoin;
+        //     this.props.fetchPrice(data);
+        // }
     } catch (error) {
         console.log(error); 
         this.props.errorAlert({
